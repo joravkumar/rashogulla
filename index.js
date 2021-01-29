55ab99a05280d67f5ecff6ef7ee9d78a545c4dd6
Jorav <joravkumar@gmail.com>
2021-01-29 10:53:03 +0530

    data types

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // alert('Hello Pallavi');
 var name = 'Pallavi';
-name = 'Jorav';
-console.log('Hello ' + name);
+var age = 29;
+var radhaage = Symbol(23);
+var pallaviage = Symbol(23);
+var no;
+var same = null;
+var isPallaviOld = false;
+var fruits = ['mango'];
+var newFunction = function () { return 'yipee' };
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof doubleage);
+console.log(typeof no);
+console.log(typeof same);
+console.log(typeof isPallaviOld);
+
+
+console.log(typeof fruits);
+console.log(typeof newFunction);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
