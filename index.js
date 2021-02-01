fc020dac8ff8870688a3bb72cd887068f2d8b1a8
Jorav <joravkumar@gmail.com>
2021-02-01 10:59:46 +0530

    strings & numbers

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+// First Session
+
 // alert('Hello Pallavi');
-var name = 'Pallavi';
+var name = "Pallavi";
 var age = 29;
 var radhaage = Symbol(23);
 var pallaviage = Symbol(23);
 var no;
 var same = null;
 var isPallaviOld = false;
-var fruits = ['mango'];
-var newFunction = function () { return 'yipee' };
+var fruits = ["mango"];
+
+var newFunction = function () {
+  return "yipee";
+};
+// Primitive
 console.log(typeof name);
 console.log(typeof age);
 console.log(typeof doubleage);
@@ -15,8 +21,22 @@ console.log(typeof no);
 console.log(typeof same);
 console.log(typeof isPallaviOld);
 
-
+// Non primitive
 console.log(typeof fruits);
 console.log(typeof newFunction);
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+
+// Second Session
+
+var str = "17";
+var convertToNum = +str;
+var num = 9;
+console.log(convertToNum + num);
+
+const problemForPallavi = 0.1 + 0.2;
+
+console.log(problemForPallavi);
+
+const test = ("ba" + +"a" + "a").toLowerCase();
+console.log(test);
